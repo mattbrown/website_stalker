00403be71b215dba4e84c7fc98f9d9294401d4f8
Matt Brown <matt.brown2@gmail.com>
2015-06-21 17:43:44 -0400

    WIP on the disney screen scrape. The selenium script works, but getting it work from python is another matter

--- stalkers.py
+++ stalkers.py
@@ -22,6 +22,27 @@ class SimpleStalker():
         return "simple"
 
 
+class DisneyRestaurantsStalker():
+    """
+    Stalk a list of restaurants for a particular date. Notify if there's a NEW value since the last check.
+
+    Has the name "disney_restaurant"
+    """
+    def __init__(self, stalk_cfg):
+        self.cfg = stalk_cfg
+        self.urls = self.cfg['urls']
+
+    def stalk(self):
+        """
+        Stalk should return a message if there is something to notify, otherwise it will return None
+        """
+        return "testing notifications"
+
+    @staticmethod
+    def name():
+        return "disney_restaurant"
+
+
 def get_stalker(stalk_cfg):
     name = stalk_cfg['name']
     if name == SimpleStalker.name():
